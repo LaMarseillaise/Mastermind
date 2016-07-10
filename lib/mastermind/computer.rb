b6c3c36a665952383293c5f46f88d7587e94f865
Andur Carr <carr.andur@gmail.com>
2016-07-10 12:52:13 -0700

    Remove Computer#initial_guess

--- lib/mastermind/computer.rb
+++ lib/mastermind/computer.rb
@@ -1,29 +1,11 @@
 module Mastermind
   class Computer < Player
     def get_guess_for(game)
-      return initial_guess if game.attempts < 1
       Code.new(sequence: Code.random(game.secret_length))
     end
 
     def get_code(length:)
       Code.new(sequence: Code.random(length))
-    end
-
-
-    private
-
-    def initial_guess
-      color1 = Piece::COLORS.sample
-      begin
-        color2 = Piece::COLORS.sample
-      end until color1 != color2
-
-      Code.new(sequence: [
-        Piece.new(color: color1),
-        Piece.new(color: color1),
-        Piece.new(color: color2),
-        Piece.new(color: color2)
-      ])
     end
   end
 end
